137884c328342b72506b6fb30871e91cb9853c0a
Juan Edwards <juan.edwards@mail.udp.cl>
2018-09-24 12:36:16 -0300

    Ahora se seleccionan los personajes y se cambian los sprites

---
+++ Assets/Scripts/changeCharacter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class changeCharacter : MonoBehaviour {
+
+    public Sprite spritek;
+    public Sprite spritem;
+    public Sprite spriteb;
+    public Sprite spriteng;
+
+    public void SelectKgnith(){
+        gameObject.GetComponent<Image>().overrideSprite = spritek;
+    }
+
+    public void SelectMage(){
+        gameObject.GetComponent<Image>().overrideSprite = spritem;
+    }
+
+    public void SelectBarbarian(){
+        gameObject.GetComponent<Image>().overrideSprite = spriteb;
+    }
+
+    public void SelectDude(){
+        gameObject.GetComponent<Image>().overrideSprite = spriteng;
+    }
+}
