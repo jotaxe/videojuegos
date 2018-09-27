4f90ac57e5a0c93faec103bbd7d9c4d7ee12f6ed
Juan Edwards <juan.edwards@mail.udp.cl>
2018-09-27 12:26:20 -0300

    Agregado boton de play (no hace nada) y cambio en los textos de los personajes a la hora de la seleccion

--- Assets/Scripts/changeCharacter.cs
+++ Assets/Scripts/changeCharacter.cs
@@ -12,19 +12,40 @@ public class changeCharacter : MonoBehaviour {
     public Sprite spriteb;
     public Sprite spriteng;
 
-    public void SelectKgnith(){
+    public void SelectKnight(){
         gameObject.GetComponent<Image>().overrideSprite = spritek;
+        gameObject.GetComponent<Image>().color = new Vector4(gameObject.GetComponent<Image>().color.r, gameObject.GetComponent<Image>().color.g, gameObject.GetComponent<Image>().color.b, 255);
+    }
+
+    public void KnightText(){
+        gameObject.GetComponent<Text>().text = "Honorable knight, with heavy armor, heavy shield, heavy sword and of course, heavy balls";
+ 
     }
 
     public void SelectMage(){
         gameObject.GetComponent<Image>().overrideSprite = spritem;
+        gameObject.GetComponent<Image>().color = new Vector4(gameObject.GetComponent<Image>().color.r, gameObject.GetComponent<Image>().color.g, gameObject.GetComponent<Image>().color.b, 255);
+    }
+
+    public void MegeText(){
+        gameObject.GetComponent<Text>().text = "Not so brilliant mage, his stronger spells is '90% Levitation' wich makes him almost levitate";
     }
 
     public void SelectBarbarian(){
         gameObject.GetComponent<Image>().overrideSprite = spriteb;
+        gameObject.GetComponent<Image>().color = new Vector4(gameObject.GetComponent<Image>().color.r, gameObject.GetComponent<Image>().color.g, gameObject.GetComponent<Image>().color.b, 255);
+    }
+
+    public void BarbarianText(){
+        gameObject.GetComponent<Text>().text = "Viking warrior with inmesurable strenght. Likes kitties.";
     }
 
     public void SelectDude(){
         gameObject.GetComponent<Image>().overrideSprite = spriteng;
+        gameObject.GetComponent<Image>().color = new Vector4(gameObject.GetComponent<Image>().color.r, gameObject.GetComponent<Image>().color.g, gameObject.GetComponent<Image>().color.b, 255);
+    }
+
+    public void DudeText(){
+        gameObject.GetComponent<Text>().text = "His name is Jimmy.";
     }
 }
