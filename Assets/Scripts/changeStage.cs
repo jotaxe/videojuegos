bed613d82a9bddff5c5c3a25a88c325e0fbce5ee
rudolph93 <rudolph1293@gmail.com>
2018-10-10 16:09:52 -0300

    A little update ooooh si

---
+++ Assets/Scripts/changeStage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class changeStage : MonoBehaviour {
+
+public Sprite highlightBack;
+	
+	// Update is called once per frame
+	public void highlightback () {
+		gameObject.GetComponent<Image>().overrideSprite = highlightBack;
+
+	}
+}
