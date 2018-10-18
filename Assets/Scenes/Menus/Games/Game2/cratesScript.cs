a88311f6f223c27a7645a3598c01186de6d492b9
Juan Edwards <juan.edwards@mail.udp.cl>
2018-10-18 12:28:54 -0300

    Entrega 1

---
+++ Assets/Scenes/Menus/Games/Game2/cratesScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cratesScript : MonoBehaviour {
+
+
+	public GameObject singleCrate;
+	public GameObject platform;
+	public Transform refPoint;
+	private float crateWidth;
+	private float platformWidth;
+	void Start () {
+		crateWidth = singleCrate.GetComponent<BoxCollider2D>().size.x;
+		platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < refPoint.position.x){
+			transform.position = new Vector3(transform.position.x + platformWidth +  2 * crateWidth, transform.position.y, transform.position.z);
+		}
+	}
+}
---
+++ Assets/Scenes/Menus/Games/Game2/orcScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orcScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	public float speed = 1.5f;
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+}
