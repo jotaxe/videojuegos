6879afa3cfc6ef988c8a31b8013b4baebcd3c9ba
rudolph93 <rudolph1293@gmail.com>
2018-10-18 11:40:04 -0300

    EligeJuego

--- Assets/Scripts/changeStage.cs
+++ Assets/Scripts/changeStage.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class changeStage : MonoBehaviour {
 
 public Sprite highlightBack;
+
+public bool cambiado = false;
+public Sprite highlightOriginal;
 	
 	// Update is called once per frame
 	public void highlightback () {
-		gameObject.GetComponent<Image>().overrideSprite = highlightBack;
+		if (cambiado == false)
+		{
+			gameObject.GetComponent<Image>().overrideSprite = highlightBack;
+			cambiado = true;
+		}
+		else
+		{
+			gameObject.GetComponent<Image>().overrideSprite = highlightOriginal;
+			cambiado = false; 
+		}
+		
 
 	}
+
+
 }
