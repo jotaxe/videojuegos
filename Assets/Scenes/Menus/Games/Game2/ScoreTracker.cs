ee89fb0a7132b747d074e91e58d1fb8d6a97b195
Juan Edwards <juan.edwards@mail.udp.cl>
2018-12-13 20:43:31 -0300

    holi

---
+++ Assets/Scenes/Menus/Games/Game2/ScoreTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour {
+    
+    public playerGameScript gameScript;
+    public Text score;
+    public bool gameEnded = false;
+	// Use this for initialization
+	void Start () {
+        GameObject Player = GameObject.FindWithTag("Player");
+        gameScript = Player.GetComponent<playerGameScript>();
+	}
+	
+	// Update is called once per frame
+    void Update () {
+        score.text = gameScript.score.ToString();
+        if(gameEnded){
+            Time.timeScale = 0;
+        }
+	}
+}
--- Assets/Scenes/Menus/Games/Game2/playerGameScript.cs
+++ Assets/Scenes/Menus/Games/Game2/playerGameScript.cs
@@ -5,18 +5,30 @@ using UnityEngine.Networking;
 
 public class playerGameScript : NetworkBehaviour {
 
+    public float speed;
+    public Transform playerTransform;
+    public ScoreTracker tracker;
+    [SyncVar]
+    public int score = 0;
+
+
 	// Use this for initialization
-	void Start () {
-		
+    void Start () {
+        Camera Cam = Camera.main;
+        tracker = Cam.GetComponent<ScoreTracker>();
 	}
 	
 	// Update is called once per frame
-	public float speed;
-    public Transform playerTransform;
-
+	
 	void Update () {
 		if(Input.GetKey(KeyCode.RightArrow)){
 			transform.position += Vector3.right * speed * Time.deltaTime;
+
+            if(!isServer){
+                return;
+            }
+
+            score += 1;
 		}
 
 		
@@ -32,5 +44,14 @@ public class playerGameScript : NetworkBehaviour {
     public override void OnStartLocalPlayer()
     {
         Camera.main.GetComponent<Follow>().SetTarget(gameObject.transform);
+        gameObject.tag = "Player";
+        Debug.Log(gameObject.tag);
     }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if(collision.gameObject.tag == "Orc"){
+            tracker.gameEnded = true;
+        }
+	}
 }
