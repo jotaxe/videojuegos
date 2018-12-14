f3820f578367878e0d30460cd8990a3f8ac5527a
antynaty <n.vergara92@gmail.com>
2018-12-13 23:34:47 -0300

    ni idea what hice

--- Assets/Scripts/changeStage.cs
+++ Assets/Scripts/changeStage.cs
@@ -6,45 +6,51 @@ using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 
-public class changeStage : NetworkBehaviour {
+public class changeStage : NetworkBehaviour
+{
 
-public Sprite highlightBack;
+    public Sprite highlightBack;
 
-static bool cambiado = false;
-public Sprite highlightOriginal;
-	
-	// Update is called once per frame
-	public void highlightback (GameObject stage) {
+    static bool cambiado = false;
+    public Sprite highlightOriginal;
 
-		if(!isServer){
-			Debug.Log("No soy server po");
-			return;
-		}
-		if(stage.name == "Game1"){
-			SceneManager.LoadScene("GameOne_Anty", LoadSceneMode.Single);
-		}
-		else if (stage.name == "Game2"){
-			SceneManager.LoadScene("GameTwo_Jota", LoadSceneMode.Single);
-		}
-		else if (stage.name == "Game3"){
-			SceneManager.LoadScene("GameTwo_Jota", LoadSceneMode.Single);
-		}
+    // Update is called once per frame
+    [ClientRpc]
+    public void RpcHighlightback(GameObject stage)
+    {
 
 
-		if (cambiado == false)
-		{
-			
-			gameObject.GetComponent<Image>().overrideSprite = highlightBack;
-			cambiado = true;
-		}
-		else
-		{
-			gameObject.GetComponent<Image>().overrideSprite = highlightOriginal;
-			cambiado = false; 
-		}
-		
+        if (stage.name == "Game1")
+        {
+            SceneManager.LoadSceneAsync("GameOne_Anty", LoadSceneMode.Additive);
+            // SceneManager.LoadScene("GameOne_Anty", LoadSceneMode.Single);
+        }
+        else if (stage.name == "Game2")
+        {
+            SceneManager.LoadSceneAsync("GameTwo_Jota", LoadSceneMode.Additive);
+            // SceneManager.LoadScene("GameTwo_Jota", LoadSceneMode.Single);
+        }
+        else if (stage.name == "Game3")
+        {
+            SceneManager.LoadSceneAsync("GameThree_Jota", LoadSceneMode.Additive);
+            // SceneManager.LoadScene("GameThree_Jota", LoadSceneMode.Single);
+        }
 
-	}
+
+        if (cambiado == false)
+        {
+
+            gameObject.GetComponent<Image>().overrideSprite = highlightBack;
+            cambiado = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().overrideSprite = highlightOriginal;
+            cambiado = false;
+        }
+
+
+    }
 
 
 }
