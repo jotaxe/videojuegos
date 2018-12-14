696e741571e91e862e6a7aaa7015e943781ea639
Juan Edwards <juan.edwards@mail.udp.cl>
2018-12-14 10:33:39 -0300

    holi

---
+++ Assets/Scripts/NetMan.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetMan : MonoBehaviour {
+
+    public NetworkManager networkManager;
+    public NetworkLobbyManager lobbyManager;
+    public InputField ip;
+
+
+
+	private void Awake()
+	{
+        networkManager = GetComponent<NetworkManager>();
+        lobbyManager = GetComponent<NetworkLobbyManager>();
+        if(lobbyManager){
+            Debug.Log(lobbyManager);  
+        }else{
+            Debug.Log("no Existe");
+        }
+
+    
+	}
+
+
+    public void StartHost(){
+        networkManager.StartHost();
+    
+    }
+
+
+
+    public void StartClient(){
+        networkManager.networkAddress = ip.text;
+        networkManager.networkPort = 7777;
+        networkManager.StartClient();
+
+    }
+
+
+    public void StartGame2(){
+        Debug.Log(lobbyManager.onlineScene);
+    }
+
+    public void StartGame3(){
+        Debug.Log(lobbyManager.onlineScene);
+    }
+
+    public void StartGame1(){
+        Debug.Log(lobbyManager.onlineScene);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
